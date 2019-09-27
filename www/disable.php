28704d9c6bd111faf51ba85f458d670d95b7b273
Tim van Dijen <tvdijen@gmail.com>
2019-09-27 14:17:02 +0200

    Create controller for the negotiate-module

--- www/disable.php
+++ www/disable.php
@@ -1,20 +1,22 @@
 <?php
+
+namespace SimpleSAML\Module\negotiate;;
+
+use SimpleSAML\Configuration;
+use SimpleSAML\Session;
+use Symfony\Component\HttpFoundation\Request;
+
 /**
  * @author Mathias Meisfjordskar, University of Oslo.
  *         <mathias.meisfjordskar@usit.uio.no>
  * @package SimpleSAMLphp
  */
 
-$params = [
-    'expire' => (mktime(0, 0, 0, 1, 1, 2038)),
-    'secure' => false,
-    'httponly' => true,
-];
-\SimpleSAML\Utils\HTTP::setCookie('NEGOTIATE_AUTOLOGIN_DISABLE_PERMANENT', 'True', $params, false);
+$config = Configuration::getInstance();
+$session = Session::getSessionFromRequest();
+$request = Request::createFromGlobals();
+$authState = $request->get('authState');
+$controller = new Controller\Negotiate($config, $session);
+$response = $controller->disable($authState);
+$response->send();
 
-$globalConfig = \SimpleSAML\Configuration::getInstance();
-$session = \SimpleSAML\Session::getSessionFromRequest();
-$session->setData('negotiate:disable', 'session', false, 86400); //24*60*60=86400
-$t = new \SimpleSAML\XHTML\Template($globalConfig, 'negotiate:disable.php');
-$t->data['url'] = \SimpleSAML\Module::getModuleURL('negotiate/enable.php');
-$t->show();
--- www/enable.php
+++ www/enable.php
@@ -1,19 +1,21 @@
 <?php
+
+namespace SimpleSAML\Module\negotiate;;
+
+use SimpleSAML\Configuration;
+use SimpleSAML\Session;
+use Symfony\Component\HttpFoundation\Request;
+
 /**
  * @author Mathias Meisfjordskar, University of Oslo.
  *         <mathias.meisfjordskar@usit.uio.no>
  * @package SimpleSAMLphp
  */
 
-$params = [
-    'secure' => false,
-    'httponly' => true,
-];
-\SimpleSAML\Utils\HTTP::setCookie('NEGOTIATE_AUTOLOGIN_DISABLE_PERMANENT', null, $params, false);
-
-$globalConfig = \SimpleSAML\Configuration::getInstance();
-$session = \SimpleSAML\Session::getSessionFromRequest();
-$session->setData('negotiate:disable', 'session', false, 86400); // 24*60*60=86400
-$t = new \SimpleSAML\XHTML\Template($globalConfig, 'negotiate:enable.php');
-$t->data['url'] = \SimpleSAML\Module::getModuleURL('negotiate/disable.php');
-$t->show();
+$config = Configuration::getInstance();
+$session = Session::getSessionFromRequest();
+$request = Request::createFromGlobals();
+$authState = $request->get('authState');
+$controller = new Controller\Negotiate($config, $session);
+$response = $controller->enable($authState);
+$response->send();
