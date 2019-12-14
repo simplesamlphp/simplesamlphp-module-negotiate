3fea6f5ab35872beb5bcbd5e200198575ec3af11
Tim van Dijen <tvdijen@gmail.com>
2019-12-14 18:14:34 +0100

    Fix PossiblyNullArgument

--- www/backend.php
+++ www/backend.php
@@ -8,6 +8,7 @@
  * @package SimpleSAMLphp
  */
 
+/** @psalm-var \SimpleSAML\Auth\State $state */
 $state = \SimpleSAML\Auth\State::loadState(
     $_REQUEST['AuthState'],
     \SimpleSAML\Module\negotiate\Auth\Source\Negotiate::STAGEID
